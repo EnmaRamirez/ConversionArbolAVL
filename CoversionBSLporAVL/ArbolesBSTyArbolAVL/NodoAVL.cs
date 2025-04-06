6124d415804ef64b749a922fd3a55596573c7765
Leticia <eramirezc38@miumg.edu.gt>
2025-04-05 23:49:21 -0600

    Arbol AVL

---
+++ CoversionBSLporAVL/ArbolesBSTyArbolAVL/NodoAVL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBSTyArbolAVL
+{
+    public class NodoAVL
+    {
+        public int data;
+        public NodoAVL left;
+        public NodoAVL right;
+        public int height;
+
+        public NodoAVL(int data)
+        {
+            this.data = data;
+            this.height = 1;
+        }
+    }
+}
